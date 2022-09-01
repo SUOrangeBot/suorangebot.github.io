bcd5a2a12f555ce4cd2c6b1c3f50eda907fd17eb
Zhuowei Zhang <zhuoweiz10@gmail.com>
2022-08-31 22:20:29 -0400

    k

--- src/Apply.js
+++ src/Apply.js
@@ -16,7 +16,7 @@ function Apply() {
       </Typography>
       
       <Typography variant="body1">
-        At the moment, Computer Vision expertise or experience with PID control is a huge plus.
+        At the moment, experience with CAD modeling, VEX/FRC/FTC, C++ coding, fund raising or designing is a plus.
       </Typography>
       <Typography variant="body1"
         sx={{
@@ -29,7 +29,7 @@ function Apply() {
       <Button
         size="large"
         variant='outlined'
-        href="https://forms.gle/5L79kFVRJh4zwgmZ9"
+        href="https://forms.gle/iASGS8kgoZGXd9g67"
         target="_blank"
         style={{
           margin: '1rem',
@@ -41,7 +41,7 @@ function Apply() {
           display: { xs:'none', md:'block' },
         }}
       >
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf4OdshWED6Dh7TSnuXRtlgwKVJkQYKsUS8PbOxZ9hDZR15Dw/viewform?embedded=true" width="640" height="2145" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe4h844jDOQCT2vChAbKgK0huAyrpUAbqEBCfu-ppVXLI7hoQ/viewform?embedded=true" width="640" height="1051" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 
       </Box>
     </Container>
